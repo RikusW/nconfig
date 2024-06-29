380d32f5a1bc214cee4f3f8e9f0a370ab717d758
Rikus Wessels <rikusw@gmail.com>
2024-06-29 21:56:13 +0200

    Reformatted header

--- kkonfigQt3/kkonfig.h
+++ kkonfigQt3/kkonfig.h
@@ -17,60 +17,67 @@ class HelpText;
 
 class KKView : public QVBox
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    KKView( int ac = 0, char **av = 0, QWidget *parent = 0, const char *name = 0 );
-    ~KKView();
+	KKView( int ac = 0, char **av = 0, QWidget *parent = 0, const char *name = 0 );
+	~KKView();
 
 protected:
-    void initFolders(const char *arch=0,const char *path=0,int ac=0,char **av=0);
+	void initFolders(const char *arch = 0, const char *path = 0, int ac = 0, char **av = 0);
 
 public slots:
-    void Search(int id);
-    void fileMenu(int id);
-    void viewMenu(int id);
-    void archMenu(int id);
-    void helpMenu(int id);
-    void ShowDeps( QListViewItem* );
-    void closeEvent(QCloseEvent *e);
+	void Search(int id);
+	void fileMenu(int id);
+	void viewMenu(int id);
+	void archMenu(int id);
+	void helpMenu(int id);
+	void ShowDeps(QListViewItem*);
+	void closeEvent(QCloseEvent *e);
 
 protected:
-    NodeRoot *nr;
-    HelpText *helptext;
-    QListView *folders,*folders2;
+	NodeRoot *nr;
+	HelpText *helptext;
+	QListView *folders, *folders2;
 };
 
 //-----------------------------------------------------------------------------
 
 class HelpText : public QTextEdit
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    HelpText(QWidget *p,const char *n) : QTextEdit(p,n)
-    { bFileNP=0; iFileStrs=-1; HelpNode=0; setPaletteBackgroundColor(QColor(150, 150, 150)); };
-    void keyPressEvent(QKeyEvent *e);
-    void setNodeRoot(NodeRoot **p) { pnr = p; };
+	HelpText(QWidget *p, const char *n) : QTextEdit(p, n)
+	{
+		bFileNP = 0;
+		iFileStrs = -1;
+		HelpNode = 0;
+		setPaletteBackgroundColor(QColor(150, 150, 150));
+	};
+	void keyPressEvent(QKeyEvent *e);
+	void setNodeRoot(NodeRoot **p) {
+		pnr = p;
+	};
 
 protected:
-    bool bFileNP;
-    int iFileStrs;
-    char FileStrs[10][255];
-    int FileLines[10];
-    Node *HelpNode;
-    NodeRoot **pnr;
+	bool bFileNP;
+	int iFileStrs;
+	char FileStrs[10][255];
+	int FileLines[10];
+	Node *HelpNode;
+	NodeRoot **pnr;
 
-    void FileInit();
-    void FilePush(char*,int,int);
-    void FileNext(int=-1);
-    void FilePrev(int=-1);
-    
+	void FileInit();
+	void FilePush(char*, int, int);
+	void FileNext(int = -1);
+	void FilePrev(int = -1);
+
 public slots:
-    void linkTo(int,int);
-    void ShowText(char *,int);
-    void ShowFile(char *,int);
-    void ShowHelp( QListViewItem* );
+	void linkTo(int, int);
+	void ShowText(char *, int);
+	void ShowFile(char *, int);
+	void ShowHelp(QListViewItem*);
 };
 
 //-----------------------------------------------------------------------------
@@ -78,22 +85,22 @@ public slots:
 class NodeListItem : public QListViewItem
 {
 public:
-    NodeListItem( QListView *p, Node *n);
-    NodeListItem( NodeListItem *parent,NodeListItem *after, Node *n );
+	NodeListItem( QListView *p, Node *n);
+	NodeListItem( NodeListItem *parent, NodeListItem *after, Node *n );
 
-    // non Qt
-    Node *node;
-    void SetIcon();
+	// non Qt
+	Node *node;
+	void SetIcon();
 
-    void activate();
-    void okRename(int col);
-    void paintCell(QPainter *p,const QColorGroup &cg,int c,int w,int a);
+	void activate();
+	void okRename(int col);
+	void paintCell(QPainter *p, const QColorGroup &cg, int c, int w, int a);
 };
 
 class NodeView : public QListView
 {
 public:
-	NodeView(QWidget *p=0,const char *n=0,WFlags f=0) : QListView(p,n,f)
+	NodeView(QWidget *p = 0, const char *n = 0, WFlags f = 0) : QListView(p, n, f)
 	{
 		QColor bcolor(150, 150, 150);
 		setPaletteBackgroundColor(bcolor);
@@ -101,12 +108,12 @@ public:
 		cgDisabled = palette().active();
 		cgSkipped  = palette().active();
 		cgDisabled.setColor(QColorGroup::Text, palette().disabled().text());
-		cgSkipped .setColor(QColorGroup::Text, QColor(200,0,200));
+		cgSkipped .setColor(QColorGroup::Text, QColor(200, 0, 200));
 		cgDisabled.setColor(QColorGroup::Background, bcolor);
 		cgSkipped .setColor(QColorGroup::Background, bcolor);
 
 	};
-	QColorGroup cgDisabled,cgSkipped;
+	QColorGroup cgDisabled, cgSkipped;
 };
 //-----------------------------------------------------------------------------
 
