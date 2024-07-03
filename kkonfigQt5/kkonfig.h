1bfc97a8f9c162c91af8ad47dfedfd5979071872
Rikus Wessels <rikusw@gmail.com>
2024-07-04 01:25:12 +0200

    Use tabs

--- kkonfigQt5/kkonfig.h
+++ kkonfigQt5/kkonfig.h
@@ -18,19 +18,19 @@ class HelpText;
 
 class KKView : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    KKView( int ac = 0, char **av = 0, QWidget *parent = 0, const char *name = 0 );
-    ~KKView();
+	KKView( int ac = 0, char **av = 0, QWidget *parent = 0, const char *name = 0 );
+	~KKView();
 
 protected:
-    void initFolders(const char *arch=0,const char *path=0,int ac=0,char **av=0);
+	void initFolders(const char *arch=0,const char *path=0,int ac=0,char **av=0);
 
 public slots:
-//    void Search(int id);
-//    void ShowDeps( QListViewItem* );
-//    void closeEvent(QCloseEvent *e);
+//	void Search(int id);
+//	void ShowDeps( QListViewItem* );
+//	void closeEvent(QCloseEvent *e);
 
 private slots:
 	void fileLoad();
@@ -62,32 +62,32 @@ protected:
 /*
 class HelpText : public QTextEdit
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    HelpText(QWidget *p,const char *n) : QTextEdit(p,n)
-    { bFileNP=0; iFileStrs=-1; HelpNode=0; };
-    void keyPressEvent(QKeyEvent *e);
-    void setNodeRoot(NodeRoot **p) { pnr = p; };
+	HelpText(QWidget *p,const char *n) : QTextEdit(p,n)
+	{ bFileNP=0; iFileStrs=-1; HelpNode=0; };
+	void keyPressEvent(QKeyEvent *e);
+	void setNodeRoot(NodeRoot **p) { pnr = p; };
 
 protected:
-    bool bFileNP;
-    int iFileStrs;
-    char FileStrs[10][255];
-    int FileLines[10];
-    Node *HelpNode;
-    NodeRoot **pnr;
+	bool bFileNP;
+	int iFileStrs;
+	char FileStrs[10][255];
+	int FileLines[10];
+	Node *HelpNode;
+	NodeRoot **pnr;
 
-    void FileInit();
-    void FilePush(char*,int,int);
-    void FileNext(int=-1);
-    void FilePrev(int=-1);
-    
+	void FileInit();
+	void FilePush(char*,int,int);
+	void FileNext(int=-1);
+	void FilePrev(int=-1);
+
 public slots:
-    void linkTo(int,int);
-    void ShowText(char *,int);
-    void ShowFile(char *,int);
-    void ShowHelp( QListViewItem* );
+	void linkTo(int,int);
+	void ShowText(char *,int);
+	void ShowFile(char *,int);
+	void ShowHelp( QListViewItem* );
 };
 */
 
@@ -99,21 +99,21 @@ public:
 	NodeListItem( QTreeWidget *p, Node *n);
 	NodeListItem( NodeListItem *parent, NodeListItem *after, Node *n );
 
-    // non Qt
+	// non Qt
 	Node *node;
 	void SetIcon();
 
-//    void activate();
-//    void okRename(int col);
-//    void paintCell(QPainter *p, const QColorGroup &cg, int c, int w, int a);
+//	void activate();
+//	void okRename(int col);
+//	void paintCell(QPainter *p, const QColorGroup &cg, int c, int w, int a);
 };
 
 /*
 class NodeView : public QListView
 {
 public:
-    NodeView(QWidget *p=0,const char *n=0,WFlags f=0) : QListView(p,n,f)
-    {
+	NodeView(QWidget *p=0,const char *n=0,WFlags f=0) : QListView(p,n,f)
+	{
 		QColor bcolor(150, 150, 150);
 		setPaletteBackgroundColor(bcolor);
 
@@ -123,8 +123,8 @@ public:
 		cgSkipped .setColor(QColorGroup::Text, QColor(200, 0, 200));
 		cgDisabled.setColor(QColorGroup::Background, bcolor);
 		cgSkipped .setColor(QColorGroup::Background, bcolor);
-    };
-    QColorGroup cgDisabled,cgSkipped;
+	};
+	QColorGroup cgDisabled,cgSkipped;
 };
 */
 //-----------------------------------------------------------------------------
